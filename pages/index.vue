64da74f28f47c482bdc6b98d7a52bdf14b4db02b
DRH <dragonfourtyseven@gmail.com>
2024-07-06 13:36:45 +0500

    testing skdrh

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,10 @@
     <h1 class="text-3xl font-bold underline text-slate-500">
       Nuxt Colab Website! Testing + Changing again
     </h1>
+    <p>Something added by SK-DRH</p>
   </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
