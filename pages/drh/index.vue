d563abc0626104faf880e64fda420de2f0580cc8
DRH <dragonfourtyseven@gmail.com>
2024-07-08 18:51:04 +0500

    adding a simple navbar

--- pages/drh/index.vue
+++ pages/drh/index.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <p>testing from skdrh</p>
-    <div>a lot of changes</div>
+    <nav class="bg-slate-200 text-slate-800 font-semibold text-lg flex gap-2 justify-between items-center p-4">
+      <span class="w-8 h-8 bg-slate-400 rounded-full"></span>
+      <ul class="flex gap-4">
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+      <span class="text-sm p-2 px-4 text-white rounded-md shadow bg-blue-500">Login</span>
+    </nav>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
 
 <style scoped></style>
