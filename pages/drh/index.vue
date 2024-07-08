d0255d77acd27e8f174fe5832e7bc83ebccdedd4
DRH <dragonfourtyseven@gmail.com>
2024-07-08 19:16:25 +0500

    updated styles

--- pages/drh/index.vue
+++ pages/drh/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <nav class="bg-slate-200 text-slate-800 font-semibold text-lg flex gap-2 justify-between items-center p-4">
-      <span class="w-8 h-8 bg-slate-400 rounded-full"></span>
+    <nav class="nav">
+      <span class="logo"></span>
       <ul class="flex gap-4">
         <li>Home</li>
         <li>Contact</li>
         <li>About</li>
       </ul>
-      <span class="text-sm p-2 px-4 text-white rounded-md shadow bg-blue-500">Login</span>
+      <span class="btn">Login</span>
     </nav>
   </div>
 </template>
@@ -15,4 +15,14 @@
 <script setup>
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav {
+  @apply bg-slate-200 text-slate-800 font-semibold text-lg flex gap-2 justify-between items-center p-4
+}
+.logo{
+  @apply w-8 h-8 bg-slate-400 rounded-full
+}
+.logo{
+  @apply text-sm p-2 px-4 text-white rounded-md shadow bg-blue-500
+}
+</style>
