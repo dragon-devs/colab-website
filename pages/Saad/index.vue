0dfca02cd26b4693ef760ebc8c42feefa9809186
Saad-Khan-11 <saadkhan11632@outlook.com>
2024-07-09 06:05:05 +0500

    Navbar and Hero Page done

--- pages/Saad/index.vue
+++ pages/Saad/index.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="mx-[16%]">
-    <header class="topbardesign grid grid-cols-6 p-4">
-      <div class="leftside">
-        <button>Home</button>
-      </div>
-      <div class="centerside">
+    <header
+      class="max-w-full bg-rose-400 text-white font-bold font-sans shadow-lg p-4 flex justify-between"
+    >
+      <button class="text-lg">Home</button>
+      <div class="flex gap-8 text-lg">
         <button>About</button>
         <button>Case Studies</button>
         <button>Team</button>
         <button>Jobs</button>
       </div>
-      <div class="rightside">
-        <button>Contact</button>
-      </div>
+      <button class="button text-lg">Contact</button>
     </header>
     <section class="grid grid-cols-2">
-      <div class="leftside bg-slate-400">
+      <div class="leftside bg-rose-100 p-6 pt-48">
         <ul>
-          <li class="w-auto font-extrabold">Programming, redefined.</li>
-          <li class="w-auto">
+          <li class="text-5xl font-extrabold font-sans">
+            Programming, redefined.
+          </li>
+          <li class="text-lg mt-4 mb-4 font-sans">
             Discover the power of a professional team and how it enables you to
             build your dreams, without limitations.
           </li>
-          <button>Schedule a Strategy Call</button>
+          <button class="button">Schedule a Strategy Call</button>
         </ul>
       </div>
       <div class="rightside">
@@ -39,10 +39,6 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-.topbardesign {
-  @apply max-w-full bg-rose-400 font-bold text-white font-sans shadow-lg;
-}
-
 .rightside {
   @apply flex col-span-1 justify-end;
 }
@@ -53,5 +49,9 @@
 
 .leftside {
   @apply flex col-span-1 justify-start;
+}
+
+.button {
+  @apply rounded-lg p-2 text-lg text-slate-100 border-2 border-rose-600 bg-rose-500 hover:bg-blue-400 hover:border-blue-500 duration-300 font-sans font-semibold text-center items-center;
 }
 </style>
--- pages/drh/index.vue
+++ pages/drh/index.vue
@@ -6,24 +6,22 @@
         <li>Home</li>
         <li>Contact</li>
         <li>About</li>
-        <li>New</li>
       </ul>
       <span class="btn">Login</span>
     </nav>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped>
 .nav {
-  @apply bg-slate-200 text-slate-800 font-semibold text-lg flex gap-2 justify-between items-center p-4
+  @apply bg-slate-200 text-slate-800 font-semibold text-lg flex gap-2 justify-between items-center p-4;
 }
-.logo{
-  @apply w-8 h-8 bg-slate-400 rounded-full
+.logo {
+  @apply w-8 h-8 bg-slate-400 rounded-full;
 }
-.logo{
-  @apply text-sm p-2 px-4 text-white rounded-md shadow bg-blue-500
+.logo {
+  @apply text-sm p-2 px-4 text-white rounded-md shadow bg-blue-500;
 }
 </style>
