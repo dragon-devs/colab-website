3ff35e525e0d2da77e5c26ac324fc5650e8b9967
Saad-Khan-11 <saadkhan11632@outlook.com>
2024-07-10 17:47:35 +0500

    added about section

--- pages/Saad/index.vue
+++ pages/Saad/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="mx-[16%]">
-    <nav
-      class="max-w-full bg-rose-400 text-white font-bold font-sans shadow-lg p-4 flex justify-between"
-    >
+    <div class="nav">
       <button class="text-lg">Home</button>
       <div class="flex gap-8 text-lg">
         <button>About</button>
@@ -11,9 +9,9 @@
         <button>Jobs</button>
       </div>
       <button class="button text-lg">Contact</button>
-    </nav>
-    <hero class="grid grid-cols-2">
-      <div class="bg-gradient-to-t from-white to-rose-100 p-6 pt-48">
+    </div>
+    <div class="hero">
+      <div class="bg-gradient-to-t from-white to-rose-100 p-6 pt-32">
         <ul>
           <li class="text-5xl font-extrabold font-sans">
             Programming, redefined.
@@ -30,19 +28,38 @@
         src="~/assets/images/hero.jpg"
         alt="Discover One Big Soft"
       />
-    </hero>
-    <about
-      class="grid grid-cols-2 bg-gradient-to-t from-rose-100 to-white h-80"
-    >
-      <div></div>
-      <div></div>
-    </about>
+    </div>
+    <div class="about">
+      <div
+        class="flex justify-center font-sans text-4xl font-extrabold mt-24 mb-4"
+      >
+        Where all dreams come into place.
+      </div>
+      <div class="flex justify-center mx-[8%]">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore modi
+        totam nihil repellendus iste corrupti odio ratione. Accusantium, vel
+        illum a laboriosam esse quasi sit, fugit magni quia minima perspiciatis.
+      </div>
+      <button class="button my-4 ml-96 w-32">Book Now</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
+.nav {
+  @apply max-w-full bg-rose-400 text-white font-bold font-sans shadow-lg p-4 flex justify-between;
+}
+
+.hero {
+  @apply grid grid-cols-2;
+}
+
+.about {
+  @apply bg-gradient-to-t from-rose-100 to-white;
+}
+
 .button {
   @apply rounded-lg p-2 text-lg text-slate-100 border-2 border-rose-600 bg-rose-500 hover:bg-blue-400 hover:border-blue-500 duration-300 font-sans font-semibold text-center items-center;
 }
